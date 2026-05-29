b07e1877a9b6db14dd183731e8b27e468acb3b86
Gabriel Roldan <groldan@multivers.io>
2026-05-29 02:11:39 -0300

    fix (c) headers

--- parquetry-core/src/main/java/io/tileverse/parquetry/data/write/page/RleBooleanEncoder.java
+++ parquetry-core/src/main/java/io/tileverse/parquetry/data/write/page/RleBooleanEncoder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2026 Tileverse.io
+ * Copyright (c) 2026 Multivers.io
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
