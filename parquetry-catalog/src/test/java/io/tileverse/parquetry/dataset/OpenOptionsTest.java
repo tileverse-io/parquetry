2188b0da0c1a8aebfd47e9148a69e6cf6a39ccb1
Gabriel Roldan <groldan@multivers.io>
2026-07-10 18:16:28 -0300

    chore: fix year in license header
    dfafcf39 mistakenly set 2025 as year in the pom.xml (c) header, fixed to
    2026.
    
    on-behalf-of: @multiversio <info@multivers.io>

--- parquetry-catalog/src/test/java/io/tileverse/parquetry/dataset/OpenOptionsTest.java
+++ parquetry-catalog/src/test/java/io/tileverse/parquetry/dataset/OpenOptionsTest.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2025 Multiversio LLC. All rights reserved.
+ * (c) Copyright 2026 Multiversio LLC. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
